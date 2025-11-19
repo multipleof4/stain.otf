f9e5c2a7d9a9cdd74cbe92c3ca7095ec10e14566
multipleof4 <planetrenox@protonmail.com>
2025-11-19 12:12:28 -0800

    Revert: Update Y.js

--- src/glyphs/Y.js
+++ src/glyphs/Y.js
@@ -1,14 +1,12 @@
 export const glyphY=(o,w)=>new o.Glyph({name:"Y",unicode:89,advanceWidth:w,path:(p=>{
   p.moveTo(20,700).lineTo(115,700).lineTo(280,350).lineTo(450,700).lineTo(545,700).lineTo(325,255).lineTo(325,0).lineTo(245,0).lineTo(245,255).close(); return p;
 })(new o.Path())});
-
 export const glypthy=(o,w)=>new o.Glyph({name:"y",unicode:121,advanceWidth:w,path:(p=>{
-  // Symmetric v shape top + consistent descender
-  p.moveTo(15,500).lineTo(95,500).lineTo(210,230).lineTo(325,500).lineTo(405,500).lineTo(250,140) // Top V
-   .lineTo(250,-60) // Straight down stem
-   .curveTo(250,-150,215,-200,135,-200).curveTo(95,-200,60,-180,35,-160) // Curve tail
-   .lineTo(60,-90).curveTo(80,-110,100,-120,135,-120).curveTo(170,-120,170,-90,170,-60)
-   .lineTo(170,140).lineTo(15,500).close(); 
+  p.moveTo(25,500).lineTo(105,500).lineTo(235,180).lineTo(365,500).lineTo(455,500).lineTo(250,0) // Main V join
+   .lineTo(195,-155) // Tail descend linear
+   .curveTo(170,-210,140,-215,105,-215).curveTo(70,-215,50,-200,30,-185) // Tail curve
+   .lineTo(55,-125).curveTo(75,-140,90,-135,110,-135).curveTo(140,-130,150,-95,165,-65).close(); // Tail terminal
   return p;
 })(new o.Path())});
+
 
