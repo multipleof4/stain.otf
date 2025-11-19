16299ba22a1e815d6a11f6ea6433798bd9ffec5d
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:17:37 -0800

    Refactor: Humanist U/u

--- src/glyphs/U.js
+++ src/glyphs/U.js
@@ -1,12 +1,6 @@
-const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
-export const glyphU=(o,w)=>mk(o,"U",85,p=>{
-  p.moveTo(80,700);p.lineTo(160,700);p.lineTo(160,240);p.curveTo(160,0,440,0,440,240);
-  p.lineTo(440,700);p.lineTo(520,700);p.lineTo(520,240);p.curveTo(520,-100,80,-100,80,240);p.close();
-  return p;
-},w);
-export const glyphu=(o,w)=>mk(o,"u",117,p=>{
-  p.moveTo(80,480);p.lineTo(160,480);p.lineTo(160,180);p.curveTo(160,80,360,80,360,180);
-  p.lineTo(360,480);p.lineTo(440,480);p.lineTo(440,0);p.lineTo(360,0);p.lineTo(360,80);
-  p.curveTo(360,-20,80,-20,80,180);p.close();
-  return p;
-},w);
+export const glyphU=(o,w)=>new o.Glyph({name:"U",unicode:85,advanceWidth:w,path:(p=>{
+  p.moveTo(85,700).lineTo(170,700).lineTo(170,240).curveTo(170,85,255,60,330,60).curveTo(435,60,505,125,505,240).lineTo(505,700).lineTo(590,700).lineTo(590,240).curveTo(590,75,475,-15,335,-15).curveTo(210,-15,85,60,85,240).close(); return p;
+})(new o.Path())});
+export const glyphu=(o,w)=>new o.Glyph({name:"u",unicode:117,advanceWidth:w,path:(p=>{
+  p.moveTo(90,500).lineTo(170,500).lineTo(170,160).curveTo(170,90,215,60,280,60).curveTo(360,60,400,120,400,220).lineTo(400,500).lineTo(480,500).lineTo(480,0).lineTo(405,0).lineTo(405,70).curveTo(365,10,305,-15,265,-15).curveTo(155,-15,90,70,90,220).close(); return p;
+})(new o.Path())});
