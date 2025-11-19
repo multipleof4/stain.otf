7fd216e45bb96e252038b601bbfe7b91047616bb
multipleof4 <planetrenox@protonmail.com>
2025-11-19 11:42:32 -0800

    Fix: glyph 't' raise crossbar to x-height for balance

--- src/glyphs/T.js
+++ src/glyphs/T.js
@@ -1,6 +1,10 @@
 export const glyphT=(o,w)=>new o.Glyph({name:"T",unicode:84,advanceWidth:w,path:(p=>{
   p.moveTo(35,635).lineTo(35,700).lineTo(525,700).lineTo(525,635).lineTo(320,635).lineTo(320,0).lineTo(240,0).lineTo(240,635).close(); return p;
 })(new o.Path())});
+
 export const glypht=(o,w)=>new o.Glyph({name:"t",unicode:116,advanceWidth:w,path:(p=>{
-  p.moveTo(145,620).lineTo(145,430).lineTo(55,430).lineTo(55,360).lineTo(145,360).lineTo(145,115).curveTo(145,40,195,-15,280,-15).curveTo(315,-15,340,-5,355,5).lineTo(350,80).curveTo(315,55,260,45,230,80).lineTo(230,360).lineTo(335,360).lineTo(335,430).lineTo(230,430).lineTo(230,620).close(); return p;
+  p.moveTo(145,630).lineTo(145,500).lineTo(40,500).lineTo(40,425).lineTo(145,425)
+   .lineTo(145,125).curveTo(145,50,190,-15,270,-15).curveTo(315,-15,340,0,355,10)
+   .lineTo(350,90).curveTo(315,65,270,60,230,95).lineTo(230,425)
+   .lineTo(335,425).lineTo(335,500).lineTo(230,500).lineTo(230,630).close(); return p;
 })(new o.Path())});
