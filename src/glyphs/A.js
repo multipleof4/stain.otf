ae1d0d816605a0e67fbef8f9ad754cc7c6a79545
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:32:20 -0800

    Fix: Refine lowercase a bowl and arch

--- src/glyphs/A.js
+++ src/glyphs/A.js
@@ -4,9 +4,9 @@ export const glyphA = (o, w) => new o.Glyph({ name: "A", unicode: 65, advanceWid
 })(new o.Path()) });
 
 export const glypha = (o, w) => new o.Glyph({ name: "a", unicode: 97, advanceWidth: w, path: (p => {
-  p.moveTo(420,280).curveTo(380,450,230,506,130,506).curveTo(50,506,25,440,25,365).curveTo(25,230,160,155,408,120).lineTo(408,100)
-   .curveTo(408,40,350,55,280,55).curveTo(180,55,120,75,120,75).lineTo(105,15).curveTo(180,-15,280,-15,340,-15)
-   .curveTo(450,-15,495,60,495,180).lineTo(495,490).lineTo(420,490).lineTo(420,280).close()
-   .moveTo(408,170).curveTo(220,200,110,260,110,350).curveTo(110,410,145,440,210,440).curveTo(330,440,408,330,408,220).close();
+  p.moveTo(420,120).lineTo(420,0).lineTo(350,0).lineTo(350,60).curveTo(310,10,260,-15,210,-15).curveTo(95,-15,35,70,35,190)
+   .curveTo(35,320,110,380,220,380).lineTo(350,380).lineTo(350,390).curveTo(350,450,300,465,230,465).curveTo(160,465,110,440,100,390)
+   .lineTo(35,410).curveTo(55,500,135,525,230,525).curveTo(380,525,420,460,420,360).lineTo(420,120).close()
+   .moveTo(350,320).lineTo(230,320).curveTo(150,320,110,270,110,190).curveTo(110,120,150,50,220,50).curveTo(290,50,350,90,350,180).close();
   return p;
 })(new o.Path()) });
