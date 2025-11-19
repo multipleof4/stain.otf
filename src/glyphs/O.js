5938107c1842dab73c4d851b1990f923bc9096f5
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:06:48 -0800

    Refactor O/o: Smooth ovals

--- src/glyphs/O.js
+++ src/glyphs/O.js
@@ -1,57 +1,11 @@
-const makeGlyph = (opentype, name, unicode, cmds, adv) =>
-  new opentype.Glyph({
-    name,
-    unicode,
-    advanceWidth: adv,
-    path: cmds(new opentype.Path())
-  });
-
-export const glyphO = (opentype, adv) =>
-  makeGlyph(
-    opentype,
-    "O",
-    "O".codePointAt(0),
-    (p) => {
-      p.moveTo(300, -10);
-      p.curveTo(160, -10, 60, 100, 60, 280);
-      p.lineTo(60, 420);
-      p.curveTo(60, 600, 160, 710, 300, 710);
-      p.curveTo(440, 710, 540, 600, 540, 420);
-      p.lineTo(540, 280);
-      p.curveTo(540, 100, 440, -10, 300, -10);
-      p.close();
-      p.moveTo(300, 50);
-      p.curveTo(410, 50, 480, 130, 480, 280);
-      p.lineTo(480, 420);
-      p.curveTo(480, 570, 410, 650, 300, 650);
-      p.curveTo(190, 650, 120, 570, 120, 420);
-      p.lineTo(120, 280);
-      p.curveTo(120, 130, 190, 50, 300, 50);
-      p.close();
-      return p;
-    },
-    adv
-  );
-
-export const glypho = (opentype, adv) =>
-  makeGlyph(
-    opentype,
-    "o",
-    "o".codePointAt(0),
-    (p) => {
-      p.moveTo(300, -10);
-      p.curveTo(180, -10, 90, 80, 90, 240);
-      p.curveTo(90, 400, 180, 490, 300, 490);
-      p.curveTo(420, 490, 510, 400, 510, 240);
-      p.curveTo(510, 80, 420, -10, 300, -10);
-      p.close();
-      p.moveTo(300, 50);
-      p.curveTo(390, 50, 450, 110, 450, 240);
-      p.curveTo(450, 370, 390, 430, 300, 430);
-      p.curveTo(210, 430, 150, 370, 150, 240);
-      p.curveTo(150, 110, 210, 50, 300, 50);
-      p.close();
-      return p;
-    },
-    adv
-  );
+const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
+export const glyphO=(o,w)=>mk(o,"O",79,p=>{
+  p.moveTo(300,710);p.curveTo(60,710,60,-10,300,-10);p.curveTo(540,-10,540,710,300,710);p.close();
+  p.moveTo(300,630);p.curveTo(460,630,460,70,300,70);p.curveTo(140,70,140,630,300,630);p.close();
+  return p;
+},w);
+export const glypho=(o,w)=>mk(o,"o",111,p=>{
+  p.moveTo(300,490);p.curveTo(80,490,80,-10,300,-10);p.curveTo(520,-10,520,490,300,490);p.close();
+  p.moveTo(300,410);p.curveTo(440,410,440,70,300,70);p.curveTo(160,70,160,410,300,410);p.close();
+  return p;
+},w);
