4e7cc6538c79f91c2b8b821d3934c22dc283f55b
multipleof4 <planetrenox@protonmail.com>
2025-11-19 11:48:53 -0800

    Fix S: smooth spine curves

--- src/glyphs/S.js
+++ src/glyphs/S.js
@@ -1,10 +1,14 @@
 export const glyphS=(o,w)=>new o.Glyph({name:"S",unicode:83,advanceWidth:w,path:(p=>{
-  p.moveTo(500,225).curveTo(495,85,415,-15,290,-15).curveTo(175,-15,100,30,60,115).lineTo(125,155)
-   .curveTo(155,95,205,65,275,65).curveTo(350,65,390,105,390,160).curveTo(390,225,325,240,265,260)
-   .curveTo(140,295,65,340,65,450).curveTo(65,590,175,715,300,715).curveTo(410,715,480,660,510,580).lineTo(440,540)
-   .curveTo(410,600,365,640,305,640).curveTo(220,640,170,600,170,520).curveTo(170,475,200,455,310,420)
-   .curveTo(445,375,505,320,505,225).close(); return p;
+  p.moveTo(500,200).curveTo(495,75,415,-15,290,-15).curveTo(175,-15,100,30,60,115).lineTo(125,155)
+   .curveTo(155,95,205,65,275,65).curveTo(350,65,410,105,410,180)
+   .curveTo(410,255,360,275,260,310) // Smoother Spine transition
+   .curveTo(140,350,70,405,70,520).curveTo(70,650,180,715,305,715)
+   .curveTo(410,715,480,660,510,580).lineTo(440,540)
+   .curveTo(410,600,365,640,305,640).curveTo(220,640,150,590,150,515)
+   .curveTo(150,455,200,420,315,380) // Spine back up
+   .curveTo(450,335,500,290,500,200).close(); return p;
 })(new o.Path())});
+
 export const glyphs=(o,w)=>new o.Glyph({name:"s",unicode:115,advanceWidth:w,path:(p=>{
   p.moveTo(390,140).curveTo(385,50,325,-15,235,-15).curveTo(145,-15,95,30,50,95).lineTo(110,135)
   .curveTo(130,85,165,55,215,55).curveTo(290,55,300,100,300,125).curveTo(300,165,285,180,235,195)
@@ -12,3 +16,4 @@ export const glyphs=(o,w)=>new o.Glyph({name:"s",unicode:115,advanceWidth:w,path
   .curveTo(320,405,285,435,230,435).curveTo(160,435,145,395,145,360).curveTo(145,335,160,305,245,275)
   .curveTo(345,245,395,200,395,140).close(); return p;
 })(new o.Path())});
+
