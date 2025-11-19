44e45406b17d1e394cebae74c819cfaf95b218f9
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:17:05 -0800

    Refactor: Evenly proportioned E/e

--- src/glyphs/E.js
+++ src/glyphs/E.js
@@ -1,14 +1,9 @@
-const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
-export const glyphE=(o,w)=>mk(o,"E",69,p=>{
-  p.moveTo(80,0);p.lineTo(80,700);p.lineTo(520,700);p.lineTo(520,620);p.lineTo(160,620);
-  p.lineTo(160,390);p.lineTo(480,390);p.lineTo(480,310);p.lineTo(160,310);p.lineTo(160,80);
-  p.lineTo(520,80);p.lineTo(520,0);p.close();
-  return p;
-},w);
-export const glyphe=(o,w)=>mk(o,"e",101,p=>{
-  p.moveTo(500,220);p.curveTo(500,490,80,490,80,240);p.curveTo(80,-10,300,-10,300,-10);
-  p.curveTo(460,-10,500,100,500,100);p.lineTo(420,120);p.curveTo(400,60,300,60,300,60);
-  p.curveTo(160,60,160,200,160,200);p.lineTo(500,200);p.close();
-  p.moveTo(160,260);p.curveTo(160,420,420,420,420,260);p.close();
-  return p;
-},w);
+export const glyphE=(o,w)=>new o.Glyph({name:"E",unicode:69,advanceWidth:w,path:(p=>{
+  p.moveTo(90,0).lineTo(90,700).lineTo(530,700).lineTo(530,625).lineTo(175,625).lineTo(175,390).lineTo(480,390).lineTo(480,315).lineTo(175,315).lineTo(175,75).lineTo(540,75).lineTo(540,0).close(); return p;
+})(new o.Path())});
+export const glyphe=(o,w)=>new o.Glyph({name:"e",unicode:101,advanceWidth:w,path:(p=>{
+  p.moveTo(475,225).curveTo(475,150,435,60,370,60).curveTo(290,60,255,100,255,150).close() // trick
+   .moveTo(470,265).lineTo(130,265).curveTo(133,180,160,-15,310,-15).curveTo(390,-15,450,20,475,90).lineTo(515,65)
+   .curveTo(480,-15,415,-80,280,-65).curveTo(145,-50,55,70,55,225).curveTo(55,385,130,506,260,506).curveTo(425,506,475,380,470,265).close()
+   .moveTo(135,305).curveTo(140,375,190,440,260,440).curveTo(355,440,400,355,400,305).close(); return p;
+})(new o.Path())});
