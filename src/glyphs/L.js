289a030e23842ab667e441c674a11bbbf4cddd8c
multipleof4 <planetrenox@protonmail.com>
2025-11-19 08:06:41 -0800

    Refactor L/l: Standard stems

--- src/glyphs/L.js
+++ src/glyphs/L.js
@@ -1,41 +1,10 @@
-const makeGlyph = (opentype, name, unicode, cmds, adv) =>
-  new opentype.Glyph({
-    name,
-    unicode,
-    advanceWidth: adv,
-    path: cmds(new opentype.Path())
-  });
-
-export const glyphL = (opentype, adv) =>
-  makeGlyph(
-    opentype,
-    "L",
-    "L".codePointAt(0),
-    (p) => {
-      p.moveTo(100, 0);
-      p.lineTo(100, 700);
-      p.lineTo(160, 700);
-      p.lineTo(160, 60);
-      p.lineTo(480, 60);
-      p.lineTo(480, 0);
-      p.close();
-      return p;
-    },
-    adv
-  );
-
-export const glyphl = (opentype, adv) =>
-  makeGlyph(
-    opentype,
-    "l",
-    "l".codePointAt(0),
-    (p) => {
-      p.moveTo(270, 0);
-      p.lineTo(270, 720);
-      p.lineTo(330, 720);
-      p.lineTo(330, 0);
-      p.close();
-      return p;
-    },
-    adv
-  );
+const mk=(o,n,u,d,w)=>new o.Glyph({name:n,unicode:u,advanceWidth:w,path:d(new o.Path())});
+export const glyphL=(o,w)=>mk(o,"L",76,p=>{
+  p.moveTo(80,0);p.lineTo(80,700);p.lineTo(160,700);p.lineTo(160,80);p.lineTo(520,80);
+  p.lineTo(520,0);p.close();
+  return p;
+},w);
+export const glyphl=(o,w)=>mk(o,"l",108,p=>{
+  p.moveTo(260,0);p.lineTo(260,710);p.lineTo(340,710);p.lineTo(340,0);p.close();
+  return p;
+},w);
